e05c4cf33b2ac1ea8e4081abffbd1f6f4ce57822
YeeSoft <makitrataras@gmail.com>
2017-12-03 13:56:02 -0500

    controller updated

--- controllers/LinkController.php
+++ controllers/LinkController.php
@@ -3,24 +3,20 @@
 namespace yeesoft\menu\controllers;
 
 use yeesoft\controllers\CrudController;
-use yii\helpers\StringHelper;
 
 /**
- * MenuLinkController implements the CRUD actions for Post model.
+ * LinkController implements the CRUD actions for Post model.
  */
 class LinkController extends CrudController
 {
     public $modelClass = 'yeesoft\models\MenuLink';
-    public $modelSearchClass = 'yeesoft\menu\models\SearchMenuLink';
     public $enableOnlyActions = ['delete', 'update', 'create'];
 
     protected function getRedirectPage($action, $model = null)
     {
         switch ($action) {
             case 'delete':
-                $searchClass = $this->modelSearchClass;
-                $formName = StringHelper::basename($searchClass::className());
-                return ['/menu/default/index', "{$formName}[menu_id]" => $model->menu_id];
+                return ['default/index', 'menu_id' => $model->menu_id];
                 break;
             case 'update':
                 return ['update', 'id' => $model->id];
